a0371d500f9a8ce164be427e286c615dcb9c0429
pudup <jasontauro@gmail.com>
2023-07-14 12:12:04 +0530

    Reduce recursion limit

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from Solver import solve
 import sys
 import time
 
-sys.setrecursionlimit(20000)
+sys.setrecursionlimit(5000)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
