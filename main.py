96c9a1260b28adf4c43952d26e70340215e9dd43
pudup <jasontauro@gmail.com>
2023-07-18 17:46:31 +0530

    Add legend

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ sys.setrecursionlimit(5000)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    print("a blocks are long vertical\n"
+          "b blocks are long horizontal\n"
+          "c blocks are the single blocks\n"
+          "d block is the goal block\n"
+          "Co-ordinates are the top left part of the block (Y,X)")
     board = Board()
     start = time.time()
     solve_BFS(board)
